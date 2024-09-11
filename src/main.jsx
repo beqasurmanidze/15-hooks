4ecd6373c13ff903d086fb3e6ae34119930b33bc
Your Name <beqasurmana2900@gmail.com>
2024-09-11 20:55:36 +0400

    feature/isOn

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,6 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-import App from "./App.jsx";
+import App2 from "./App2";
 import "./index.css";
 
-createRoot(document.getElementById("root")).render(<App />);
+createRoot(document.getElementById("root")).render(<App2 />);
